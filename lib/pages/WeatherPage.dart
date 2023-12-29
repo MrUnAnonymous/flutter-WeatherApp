a6c842c29c37effe3a49c14410c484b4c8703213
MrUnAnonymous <saurabhsapkal16@gmail.com>
2023-12-29 14:28:04 +0530

    added fetch data from API

--- lib/pages/WeatherPage.dart
+++ lib/pages/WeatherPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +10,47 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  //api key
+  final _weatherService = WeatherService('cce4281d72e34b0107d4a8b3158107dc');
+  Weather? _weather;
+
+  //fetch weather
+  _fetchWeather() async {
+    //get current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+  //weather animations
+
+  //init state
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? ""),
+            Text('${_weather?.temp.round()}°C'),
+          ],
+        ),
+      ),
+    );
   }
 }
